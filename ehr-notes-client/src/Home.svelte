c945d88a317416a419bf592f008d1f68bbb74938
Thor Stenbæk <tst@dips.no>
2021-03-16 11:47:47 +0100

    Cleaned dead code

--- ehr-notes-client/src/Home.svelte
+++ ehr-notes-client/src/Home.svelte
@@ -1,13 +1,8 @@
 <script>  
     import SmartOnFhir from "./components/SmartOnFhir.svelte";
     import Document from "./components/Document.svelte";
-    
-    function beforeunload(event) {        
-        console.log("close" + event);        
-    }
-
 </script>
 
-<svelte:window on:beforeunload={beforeunload}/>
 <SmartOnFhir/>
 <Document/>
+
